66a80feb8b14db3395650f6c060832e539aadde8
Tonyjlum <tonyjlum@gmail.com>
2018-12-26 21:18:24 -0500

    change to the session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
   def create
-    if !params[:name] || params[:name].empty?
+    if params[:name].nil? || params[:name].empty?
       redirect_to controller: 'sessions', action: 'new'
     else
       session[:name] = params[:name]
